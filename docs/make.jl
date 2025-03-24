602359e471e471be35ab98563de2e5257d8eea82
Rafael <rafaelab@users.noreply.github.com>
2025-03-24 10:58:53 +0100

    add makefile to generate documentation automatically

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter
+
+push!(LOAD_PATH, "..")
+
+using AstroCoordinates
+
+DocMeta.setdocmeta!(AstroCoordinates, :DocTestSetup, :(using AstroCoordinates))
+
+
+makedocs(
+	sitename = "AstroCoordinates.jl",
+	format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+	modules = [AstroCoordinates],
+	workdir = joinpath(@__DIR__, ".."),
+	pages = [
+		"Home" => "index.md",
+	]
+)
+
+deploydocs(repo = "github.com/rafaelab/AstroCoordinates.jl.git", versions = nothing)
